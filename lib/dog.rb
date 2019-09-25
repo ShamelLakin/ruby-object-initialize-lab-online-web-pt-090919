714ab73c2a5c31efaec40eece0950fbb4c3ca58d
Shamel Lakin <christlife4u@gmail.com>
2019-09-25 19:55:36 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog 
+  def initialize(breed)
+    @breed = breed
+ end
+ 
+ def breed=(breed)
+   @breed = breed
+ end 
+ 
+ def breed 
+   @breed
+ end
+ 
+ def initialize(name) 
+   @name = name
+ 
+ def name=(name)
+   @name = name
+ end 
+ 
+ def name 
+   @name
+ end
+end
+
+Dog.new("Mutt")
+
